22d79ef5e93f54e667601c8c5e77a3e685774912
shahriaarrr <shahriaarrr@gmail.com>
2023-10-20 14:54:22 +0330

    add profile model

--- shazam/models.py
+++ shazam/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#create profile for users
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hunters = models.ManyToManyField('self', related_name="hunted_by", symmetrical=False, blank=True)
