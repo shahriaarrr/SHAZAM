1119b519a076bd55793bed7a3e52a50d2538fab5
shahriaarrr <shahriaarrr@gmail.com>
2023-10-20 14:57:33 +0330

    return username of each profile

--- shazam/models.py
+++ shazam/models.py
@@ -6,3 +6,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     hunters = models.ManyToManyField('self', related_name="hunted_by", symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.user.username
