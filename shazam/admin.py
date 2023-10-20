7e694108c1839eb427b93c2fcd20813bb29e4a63
shahriaarrr <shahriaarrr@gmail.com>
2023-10-20 15:05:53 +0330

    mixed profile info into user info

--- shazam/admin.py
+++ shazam/admin.py
@@ -6,14 +6,20 @@ from .models import Profile
 #remove group
 admin.site.unregister(Group)
 
+# add profile info into user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
 # Extand user model
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
+    inlines = [ProfileInline]
 
 # remove initial users
 admin.site.unregister(User)
 
 #register models
 admin.site.register(User, UserAdmin)
-admin.site.register(Profile)
+
+
