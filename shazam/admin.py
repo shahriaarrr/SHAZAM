f63445789d41ffd0212455d52a73293777f352f3
shahriaarrr <shahriaarrr@gmail.com>
2023-10-20 12:32:39 +0330

    add UserAmdin model

--- shazam/admin.py
+++ shazam/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 # Register your models here.
 
 #remove group
 admin.site.unregister(Group)
+
+# Extand user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+
+# remove initial users
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
