f2d6dd7f72b6443e1f75bd53ef2d3bbaf8b58d5d
shahriaarrr <shahriaarrr@gmail.com>
2023-10-20 15:31:12 +0330

    add signal to create profile for new users

--- shazam/apps.py
+++ shazam/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ShazamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shazam'
+
+    def ready(self):
+        import shazam.signals
---
+++ shazam/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.hunters.set([instance.profile.id])
+        user_profile.save()
+
