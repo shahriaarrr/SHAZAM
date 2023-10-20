a594d475e263e43818d610325652e7cbbbb0b4bb
shahriaarrr <shahriaarrr@gmail.com>
2023-10-20 14:57:55 +0330

    register profile in admin panel

--- shazam/admin.py
+++ shazam/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 # Register your models here.
 
 #remove group
@@ -13,4 +14,6 @@ class UserAdmin(admin.ModelAdmin):
 # remove initial users
 admin.site.unregister(User)
 
+#register models
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
