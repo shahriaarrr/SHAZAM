2d8b7f23b903d4ad4e7b3899e971da3bff36b79e
shahriaarrr <shahriaarrr@gmail.com>
2023-10-20 17:43:50 +0330

    add profile list page

--- shazam/urls.py
+++ shazam/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('profile_list/', views.profile_list, name='profile_list'),
 ]
--- shazam/views.py
+++ shazam/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def profile_list(request):
+    return render(request, 'profile_list.html', {})
