1ce42a9fef2143df11eb8ab0701f7817e43b05c1
shahriaarrr <shahriaarrr@gmail.com>
2023-10-21 12:26:54 +0330

    add joining date of each user to profile

--- shazam/models.py
+++ shazam/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     hunters = models.ManyToManyField('self', related_name="hunted_by", symmetrical=False, blank=True)
+    date_modified = models.DateTimeField(User, auto_now=True)
 
     def __str__(self):
         return self.user.username
