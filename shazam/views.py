79ae6137bb19eb6551c7b5515b5f2e5901c91f90
shahriaarrr <shahriaarrr@gmail.com>
2023-10-21 12:31:44 +0330

    add profile list view to show all profiles

--- shazam/views.py
+++ shazam/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {})
 
 def profile_list(request):
-    return render(request, 'profile_list.html', {})
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, 'profile_list.html', {"profiles": profiles})
