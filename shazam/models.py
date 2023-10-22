c985e131cdd997642835e9e8d9bef2ee97d543ef
shahriaarrr <shahriaarrr@gmail.com>
2023-10-22 15:17:19 +0330

    add thunder model

--- shazam/models.py
+++ shazam/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Thunder(models.Model):
+    user = models.ForeignKey(User, related_name="thunder", on_delete=models.CASCADE)
+    text = models.CharField(max_length=313)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user}"
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+        )
+
+
 #create profile for users
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
