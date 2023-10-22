f3a8b2a7a25e941bc33bc52d480318df0d068c3f
shahriaarrr <shahriaarrr@gmail.com>
2023-10-22 15:02:41 +0330

    add follow and unfollow actions

--- shazam/views.py
+++ shazam/views.py
@@ -17,6 +17,19 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+
+        #post form
+        if request.method == "POST":
+            current_user_profile = request.user.profile
+            action = request.POST['hunt']
+
+            if action == "unhunt":
+                current_user_profile.hunters.remove(profile)
+            else:
+                current_user_profile.hunters.add(profile)
+
+            current_user_profile.save()
+
         return render(request, 'profile.html', {'profile': profile})
     else:
         messages.success(request, ("you must Be logged In to see this page..."))
