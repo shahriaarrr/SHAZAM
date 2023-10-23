20c8a765e2be98e84245f2bb7429aba93ac925c3
shahriaarrr <shahriaarrr@gmail.com>
2023-10-23 09:57:11 +0330

    remove () from view function

--- shazam/urls.py
+++ shazam/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('profile_list/', views.profile_list, name='profile_list'),
     path('profile/<int:pk>', views.profile, name='profile'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout(), name='logout'),
+    path('logout/', views.logout, name='logout'),
 ]
