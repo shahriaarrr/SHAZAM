60900146642a3bdba6ad3a3fcf0cd8bcdebb0920
shahriaarrr <shahriaarrr@gmail.com>
2023-10-23 22:28:16 +0330

    complete login view

--- shazam/views.py
+++ shazam/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, logout
 from .models import Profile, Thunder
 from .forms import ThunderForm
 # Create your views here.
@@ -56,7 +57,19 @@ def profile(request, pk):
         return redirect('home')
 
 def login(request):
-    return render(request, 'login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, (f"Welcome back {username}, let's hunt :)"))
+            return redirect('home')
+        else:
+            messages.error(request, (f"There was an error in your username or password. please try again"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
 
 def logout(request):
     return render(request, 'logout.html', {})
