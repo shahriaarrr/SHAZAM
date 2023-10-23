cd8f61c1d39e83066a0e18ca7228ddc00920582b
shahriaarrr <shahriaarrr@gmail.com>
2023-10-23 09:19:37 +0330

    add thunder form in home page view

--- shazam/views.py
+++ shazam/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Thunder
+from .forms import ThunderForm
 # Create your views here.
 
 def home(request):
     if request.user.is_authenticated:
+        form = ThunderForm(request.POST or None)
+        if request.method == "POST":
+            if form.is_valid():
+                thunder = form.save(commit=False)
+                thunder.user = request.user
+                thunder.save()
+
+                messages.success(request, ("⚡Your thunder has been successfully struck⚡"))
+
+                return redirect('home')
+
+
         thunders = Thunder.objects.all().order_by("-created_at")
-    return render(request, 'home.html', {'thunders': thunders})
+        return render(request, 'home.html', {'thunders': thunders})
+    else:
+        thunders = Thunder.objects.all().order_by("-created_at")
+        return render(request, 'home.html', {'thunders': thunders})
 
 def profile_list(request):
     if request.user.is_authenticated:
