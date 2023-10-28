98a1f134eb4a11367a2ac4fb50698362d02a2a9c
shahriaarrr <shahriaarrr@gmail.com>
2023-10-28 10:39:13 +0330

    add profile image field

--- shazam/models.py
+++ shazam/models.py
@@ -20,6 +20,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     hunters = models.ManyToManyField('self', related_name="hunted_by", symmetrical=False, blank=True)
     date_modified = models.DateTimeField(User, auto_now=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='images/')
 
     def __str__(self):
         return self.user.username
